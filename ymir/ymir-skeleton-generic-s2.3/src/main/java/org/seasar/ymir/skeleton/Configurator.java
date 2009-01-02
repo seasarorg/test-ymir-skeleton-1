4173263a9a403cdd91b672c250bcb362f25be10b
YOKOTA, Takehiko <skirnir@gmail.com>
2009-01-02 07:51:01 +0000

    Ymir-1.0.x用に変更した。
    git-svn-id: https://www.seasar.org/svn/ymir/skeleton/trunk@2502 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir/ymir-skeleton-generic-s2.3/src/main/java/org/seasar/ymir/skeleton/Configurator.java
+++ ymir/ymir-skeleton-generic-s2.3/src/main/java/org/seasar/ymir/skeleton/Configurator.java
@@ -1,5 +1,8 @@
 package org.seasar.ymir.skeleton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IProject;
 import org.seasar.kvasir.util.PropertyUtils;
 import org.seasar.kvasir.util.collection.MapProperties;
@@ -17,13 +20,21 @@ public class Configurator extends AbstractConfigurator {
 
     private static final String ARTIFACTID_YMIR = "ymir-extension";
 
+    private static final String PREFIX_AVAILABLE_VERSION = "1.0.";
+
     @Override
     public void start(IProject project, ViliBehavior behavior,
             ViliProjectPreferences preferences) {
         MapProperties properties = behavior.getProperties();
 
-        String[] versions = Activator.getArtifactResolver().getVersions(
-                GROUPID_YMIR, ARTIFACTID_YMIR, true);
+        List<String> list = new ArrayList<String>();
+        for (String version : Activator.getArtifactResolver().getVersions(
+                GROUPID_YMIR, ARTIFACTID_YMIR, true)) {
+            if (version.startsWith(PREFIX_AVAILABLE_VERSION)) {
+                list.add(version);
+            }
+        }
+        String[] versions = list.toArray(new String[0]);
 
         properties.setProperty(KEY_PRODUCTVERSION_CANDIDATES, PropertyUtils
                 .join(versions));
