2547f13a273a9a946290f846ce18cdc41c007b9d
YOKOTA, Takehiko <skirnir@gmail.com>
2009-01-09 13:50:13 +0000

    ・追加時にDBFluteのクラスがないエラーが出ないようにした。 ・mapping+.diconが適切にマージされるようにした。
    git-svn-id: https://www.seasar.org/svn/ymir/skeleton/trunk@2606 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ysp/ysp-base/src/main/skeleton/src/main/java/${rootPackagePath}/dbflute/allcommon/exception/EntityAlreadyDeletedException.java
@@ -0,0 +1,6 @@
+package ${rootPackageName}.dbflute.allcommon.exception;
+
+// This class will be deleted by DBFlute.
+public class EntityAlreadyDeletedException extends Exception {
+    private static final long serialVersionUID = 1L;
+}
