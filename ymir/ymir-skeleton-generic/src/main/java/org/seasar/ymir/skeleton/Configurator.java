66d5ccd9df9027eda0257006b7301f3dd594dbdb
YOKOTA, Takehiko <skirnir@gmail.com>
2009-02-09 06:49:09 +0000

    ymir-1.0.2（正確にはymir-zpt-1.0.2）のためのリソースを追加したため、一覧表示で1.0.2以上のymirしか表示されないようにした。
    git-svn-id: https://www.seasar.org/svn/ymir/skeleton/trunk@2779 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir/ymir-skeleton-generic/src/main/java/org/seasar/ymir/skeleton/Configurator.java
+++ ymir/ymir-skeleton-generic/src/main/java/org/seasar/ymir/skeleton/Configurator.java
@@ -10,27 +10,30 @@ import org.seasar.ymir.vili.AbstractConfigurator;
 import org.seasar.ymir.vili.Activator;
 import org.seasar.ymir.vili.ViliBehavior;
 import org.seasar.ymir.vili.ViliProjectPreferences;
+import org.seasar.ymir.vili.maven.util.ArtifactUtils;
 
 public class Configurator extends AbstractConfigurator {
     private static final String KEY_PRODUCTVERSION_CANDIDATES = "template.parameter.productVersion.candidates";
 
     private static final String KEY_PRODUCTVERSION_DEFAULT = "template.parameter.productVersion.default";
 
+    private static final String KEY_YMIR_PREREQUISITE = "ymir.prerequisite";
+
     private static final String GROUPID_YMIR = "org.seasar.ymir";
 
     private static final String ARTIFACTID_YMIR = "ymir-extension";
-
-    private static final String PREFIX_AVAILABLE_VERSION = "1.0.";
 
     @Override
     public void start(IProject project, ViliBehavior behavior,
             ViliProjectPreferences preferences) {
         MapProperties properties = behavior.getProperties();
 
+        String prerequisite = behavior.getProperties().getProperty(
+                KEY_YMIR_PREREQUISITE);
         List<String> list = new ArrayList<String>();
         for (String version : Activator.getArtifactResolver().getVersions(
                 GROUPID_YMIR, ARTIFACTID_YMIR, false)) {
-            if (version.startsWith(PREFIX_AVAILABLE_VERSION)) {
+            if (ArtifactUtils.compareVersions(version, prerequisite) >= 0) {
                 list.add(version);
             }
         }
