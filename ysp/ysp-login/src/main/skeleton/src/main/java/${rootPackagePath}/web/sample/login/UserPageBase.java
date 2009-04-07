a74b03a25a3831167b51719d4dcf623a765ec418
YOKOTA, Takehiko <skirnir@gmail.com>
2009-04-07 01:57:15 +0000

    パッケージ名がおかしかったので修正した。
    git-svn-id: https://www.seasar.org/svn/ymir/skeleton/trunk@3104 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ysp/ysp-login/src/main/skeleton/src/main/java/${rootPackagePath}/web/sample/login/UserPageBase.java
+++ ysp/ysp-login/src/main/skeleton/src/main/java/${rootPackagePath}/web/sample/login/UserPageBase.java
@@ -1,7 +1,7 @@
 package ${rootPackageName}.web.sample.login;
 
 //TODO サンプルです。不要であれば削除して下さい。
-public class UserPageBase extends yspnew.web.PageBase {
+public class UserPageBase extends ${rootPackageName}.web.PageBase {
     public void _get() {
 
     }
