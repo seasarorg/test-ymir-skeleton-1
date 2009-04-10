f414b07ceb3b528c1a7044ce04f64d03b99d4e5a
YOKOTA, Takehiko <skirnir@gmail.com>
2009-04-10 08:49:23 +0000

    最新のVili-apiにあわせた。
    git-svn-id: https://www.seasar.org/svn/ymir/skeleton/trunk@3170 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir/ymir-fragment-amf/src/main/java/org/seasar/ymir/vili/skeleton/Configurator.java
+++ ymir/ymir-fragment-amf/src/main/java/org/seasar/ymir/vili/skeleton/Configurator.java
@@ -6,8 +6,8 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.t2framework.vili.AbstractConfigurator;
-import org.t2framework.vili.Activator;
 import org.t2framework.vili.ViliBehavior;
+import org.t2framework.vili.ViliContext;
 import org.t2framework.vili.ViliProjectPreferences;
 import org.t2framework.vili.model.maven.Dependency;
 
@@ -23,13 +23,13 @@ public class Configurator extends AbstractConfigurator {
             ViliProjectPreferences preferences, Map<String, Object> parameters,
             IProgressMonitor monitor) {
         try {
-            Dependency dependency = Activator.getProjectBuilder()
+            Dependency dependency = ViliContext.getVili().getProjectBuilder()
                     .getDependency(project, GROUPID_YMIR, ARTIFACTID_YMIR);
             if (dependency != null) {
                 parameters.put(KEY_PRODUCTVERSION, dependency.getVersion());
             }
         } catch (CoreException ex) {
-            Activator.log(ex);
+            ViliContext.getVili().log(ex);
             throw new RuntimeException(ex);
         }
     }
