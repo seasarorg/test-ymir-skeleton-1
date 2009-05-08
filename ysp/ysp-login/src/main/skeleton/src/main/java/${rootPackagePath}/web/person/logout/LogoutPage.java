819f362c91ba24fec1c7caa274aff4c9f7c9ecb2
YOKOTA, Takehiko <skirnir@gmail.com>
2009-05-08 14:13:15 +0000

    ログアウトの際のセッションの無効化の処理を@InvalidateSessionアノテーションを使って行なうようにした。
    git-svn-id: https://www.seasar.org/svn/ymir/skeleton/trunk@3312 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ysp/ysp-login/src/main/skeleton/src/main/java/${rootPackagePath}/web/person/logout/LogoutPage.java
+++ ysp/ysp-login/src/main/skeleton/src/main/java/${rootPackagePath}/web/person/logout/LogoutPage.java
@@ -1,23 +1,15 @@
 package ${rootPackageName}.web.person.logout;
 
 import org.seasar.ymir.Response;
-import org.seasar.ymir.scope.annotation.Inject;
-import org.seasar.ymir.session.SessionManager;
+import org.seasar.ymir.session.annotation.InvalidateSession;
 
 import ${rootPackageName}.web.person.login.LoginPage;
 import ${rootPackageName}.ymir.util.Redirect;
 
 public class LogoutPage extends LogoutPageBase {
-    private SessionManager sessionManager;
-
-    @Inject
-    public void setSessionManager(SessionManager sessionManager) {
-        this.sessionManager = sessionManager;
-    }
-
     @Override
+    @InvalidateSession
     public Response _get() {
-        sessionManager.invalidateSession();
         return Redirect.to(LoginPage.class);
     }
 }
